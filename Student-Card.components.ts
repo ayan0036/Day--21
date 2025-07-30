6606b814839ff487c9d8d57c8d3add767a528f3e
Pathan Ayan khan <ayankhan782204@gmail.com>
2025-07-30 09:44:42 +0530

    Create Student-Card.components.ts

---
+++ Student-Card.components.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-student-card',
+  templateUrl: './student-card.component.html',
+  styleUrls: ['./student-card.component.css']
+})
+export class StudentCardComponent {
+  @Input() name!: string;
+  @Input() course!: string;
+}
